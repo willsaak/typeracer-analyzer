5ac5559cde2b24eeeebed05e10d22ef7f88c0da1
willsaak <willsaak@gmail.com>
2021-04-19 14:24:15 +0200

    add downloading users histories

--- scrapers/userdata.py
+++ scrapers/userdata.py
@@ -1,4 +1,5 @@
 import urllib.request
+import urllib.error
 import json
 
 
@@ -7,8 +8,17 @@ class UserDataScraper:
         self._username = username
 
     def get_data(self):
-        url = f'https://data.typeracer.com/games?playerId=tr:{self._username}&universe=play&startDate=1095166400&endDate=2495252800'
-        with urllib.request.urlopen(url) as url_data:
-            data = json.loads(url_data.read().decode())
+        all_data = []
+        start_date = '1000000000'
+        end_date = '2000000000'
+        while True:
+            url = f'https://data.typeracer.com/games?playerId=tr:{self._username}&universe=play&startDate={start_date}&endDate={end_date}'
+            try:
+                with urllib.request.urlopen(url) as url_data:
+                    data = json.loads(url_data.read().decode())
+                    end_date = data[-1]['t'] - 0.01
+                    all_data.extend(data)
+            except urllib.error.HTTPError:
+                break
 
-        return data
+        return all_data
--- scripts/download_user_history.py
+++ scripts/download_user_history.py
@@ -6,19 +6,25 @@ from utils.data_converter import DataConverter
 from tqdm import tqdm
 
 
-def main():
+def download_user_histories():
     print('Downloading history...')
-    users_histories = pd.DataFrame()
     with open('../data/usernames.pkl', 'rb') as file:
         usernames = pickle.load(file)
-        for username in tqdm(usernames):
-            data = UserDataScraper(username).get_data()
-            data = DataConverter(data).get_data()
-            data['username'] = username
-            users_histories = users_histories.append(data)
-    print('Saving history...')
-    with open('../data/users_histories.pkl', 'wb') as file:
-        pickle.dump(users_histories, file)
+    for username in tqdm(usernames):
+        data = UserDataScraper(username).get_data()
+        data = DataConverter(data).get_data()
+        data['username'] = username
+        with open(f'../data/user_histories/{username}_history.pkl', 'wb') as file:
+            pickle.dump(data, file)
+
+
+def combine_user_histories():
+    print('Combining histories...')
+
+
+def main():
+    download_user_histories()
+    combine_user_histories()
 
 
 if __name__ == '__main__':
